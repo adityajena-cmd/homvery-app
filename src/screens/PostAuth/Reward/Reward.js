ab0a0b2ed7957d84913cfefbe1836e2665a41f4c
ADI <94536196+adityajena-cmd@users.noreply.github.com>
2022-01-31 07:27:09 +0530

    reward

--- src/screens/PostAuth/Reward/Reward.js
+++ src/screens/PostAuth/Reward/Reward.js
@@ -5,6 +5,27 @@ import Modal from "react-native-modal";
 import Fontisto from 'react-native-vector-icons/Fontisto'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+const width = Dimensions.get('screen').width
+const RewardCard = () => {
+    return (
+        <View style={{ elevation: 5, width: width / 2 - 30, height: width / 4, backgroundColor: '#ffffff', borderRadius: 7, marginBottom: 20 }}>
+            <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingTop: 10,
+                paddingLeft: 10
+            }}>
+                <Image source={require('../../../assets/rewardCoin.png')} style={{ width: width / 15, height: width / 15, }} resizeMode='contain' />
+                <View style={{ paddingLeft: 10, zIndex: 5 }}>
+                    <Text style={{ color: '#635E5E', fontSize: 20, fontWeight: '700' }}>10<Text style={{ color: '#635E5E', fontSize: 15, fontWeight: '400' }}> Coins</Text></Text>
+                    <Text style={{ color: '#635E5E', fontSize: 8, fontWeight: '400' }}>12 Nov 2021</Text>
+                </View>
+            </View>
+            <Text style={{ color: '#635E5E', fontSize: 8, fontWeight: '400', position: 'absolute', bottom: 10, left: 10 }}>BH2908769</Text>
+            <Image source={require('../../../assets/rewardGiftImage.png')} style={{ width: width / 5, height: width / 6, position: 'absolute', bottom: 0, right: 10, zIndex: 1 }} resizeMode='contain' />
+        </View>
+    );
+}
 
 export default function Reward({ navigation }) {
     const width = Dimensions.get('screen').width
@@ -22,59 +43,76 @@ export default function Reward({ navigation }) {
             <ScrollView showsVerticalScrollIndicator={false}>
                 <View style={{ paddingHorizontal: 20, paddingTop: 10 }}>
                     <View style={{ paddingHorizontal: 10, backgroundColor: 'white', marginTop: 15 }}>
-                    <View style={{ backgroundColor: '#ffffff', elevation: 5, paddingHorizontal: 20, paddingVertical: 10, borderRadius: 10, marginBottom: 10 }}>
-                        <View style={{ paddingVertical: 10 }}>
-                            <View style={{ flexDirection: 'row', justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
-                                <View style={{ flex: 1 }}>
-                                    <View style={{ flexDirection: 'row', alignContent: 'center', alignItems: 'center', justifyContent: 'center' }}>
-                                        <Text style={{ fontSize: 15, color: '#000000', fontWeight: '700' }}>Total coins earned</Text>
-                                    </View>
-                                    <View style={{ flexDirection: 'row', alignContent: 'center', alignItems: 'center', justifyContent: 'flex-start' }}>
-                                        <Image source={require('../../../assets/rewards5.png')} style={{width: width/6, height: width/6}} resizeMode='contain'/>
-                                        <View style={{ marginLeft: 10 }}>
-                                            <Text style={{ fontSize: 30, color: '#000000', fontWeight: '700' }}>472</Text>
+                        <View style={{ backgroundColor: '#ffffff', elevation: 5, paddingHorizontal: 20, paddingVertical: 10, borderRadius: 10, marginBottom: 10 }}>
+                            <View style={{ paddingVertical: 10 }}>
+                                <View style={{ flexDirection: 'row', justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
+                                    <View style={{ flex: 1 }}>
+                                        <View style={{ flexDirection: 'row', alignContent: 'center', alignItems: 'center', justifyContent: 'center' }}>
+                                            <Text style={{ fontSize: 15, color: '#000000', fontWeight: '700' }}>Total coins earned</Text>
+                                        </View>
+                                        <View style={{ flexDirection: 'row', alignContent: 'center', alignItems: 'center', justifyContent: 'flex-start' }}>
+                                            <Image source={require('../../../assets/rewards5.png')} style={{ width: width / 6, height: width / 6 }} resizeMode='contain' />
+                                            <View style={{ marginLeft: 10 }}>
+                                                <Text style={{ fontSize: 30, color: '#000000', fontWeight: '700' }}>472</Text>
+                                            </View>
                                         </View>
                                     </View>
-                                </View>
-                                <View style={{ width: 2, height: '100%', backgroundColor: '#DCEBF7', marginHorizontal: 10 }} />
-                                <View style={{ flex: 1 }}>
-                                    <View style={{ flexDirection: 'row', alignContent: 'center', alignItems: 'center', justifyContent: 'center' }}>
-                                        <Text style={{ fontSize: 15, color: '#000000', fontWeight: '700' }}>Total coins Saved</Text>
-                                    </View>
-                                    <View style={{ flexDirection: 'row', alignContent: 'center', alignItems: 'center', justifyContent: 'flex-start' }}>
-                                        <Image source={require('../../../assets/rewards4.png')} style={{width: width/6, height: width/6}} resizeMode='contain'/>
-                                        <View style={{ marginLeft: 10 }}>
-                                            <Text style={{ fontSize: 30, color: '#000000', fontWeight: '700' }}>472</Text>
+                                    <View style={{ width: 2, height: '100%', backgroundColor: '#DCEBF7', marginHorizontal: 10 }} />
+                                    <View style={{ flex: 1 }}>
+                                        <View style={{ flexDirection: 'row', alignContent: 'center', alignItems: 'center', justifyContent: 'center' }}>
+                                            <Text style={{ fontSize: 15, color: '#000000', fontWeight: '700' }}>Total coins Saved</Text>
+                                        </View>
+                                        <View style={{ flexDirection: 'row', alignContent: 'center', alignItems: 'center', justifyContent: 'flex-start' }}>
+                                            <Image source={require('../../../assets/rewards4.png')} style={{ width: width / 6, height: width / 6 }} resizeMode='contain' />
+                                            <View style={{ marginLeft: 10 }}>
+                                                <Text style={{ fontSize: 30, color: '#000000', fontWeight: '700' }}>472</Text>
+                                            </View>
                                         </View>
                                     </View>
                                 </View>
                             </View>
-                        </View>
-                        <View style={{ height: 2, backgroundColor: '#DCEBF7', marginTop: 10 }} />
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+                            <View style={{ height: 2, backgroundColor: '#DCEBF7', marginTop: 10 }} />
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
        
-                            <View style={{ flexDirection: 'row', justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
-                                <Text style={{ fontSize: 12, color: '#635E5E', fontWeight: '600' }}>Tricks to earn more coins</Text>
+                                <View style={{ flexDirection: 'row', justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
+                                    <Text style={{ fontSize: 12, color: '#635E5E', fontWeight: '600' }}>Tricks to earn more coins</Text>
+                                </View>
+                                <Button
+                                    onPress={() => { setModal(true) }}
+                                    style={{ width: 'auto', fontSize: 20, borderWidth: 1, borderColor: '#05194E', borderRadius: 10, paddingVertical: 0 }}
+                                    mode="outlined">
+                                    <Text style={{ color: '#05194E', fontSize: 12, fontWeight: '400' }}>View Tricks</Text>
+                                </Button>
                             </View>
-                            <Button
-                                onPress={() => { setModal(true) }}
-                                style={{ width: 'auto', fontSize: 20, borderWidth: 1,borderColor: '#05194E', borderRadius: 10, paddingVertical: 0 }}
-                                mode="outlined">
-                                <Text style={{ color: '#05194E', fontSize: 12, fontWeight: '400' }}>View Tricks</Text>
-                            </Button>
                         </View>
                     </View>
-                </View>
                     <Image source={require('../../../assets/rewards1.png')} style={{ width: width - 40, height: width / 3.2 }} resizeMode='contain' />
-                    <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between', alignContent: 'center', alignItems: 'center' }}>
-                        <TouchableOpacity onPress={()=>{setRewardModal(true)}}><Image source={require('../../../assets/rewards2.png')} style={{ width: width / 2-20, height: width / 3 }} resizeMode='contain' /></TouchableOpacity>
-                        <TouchableOpacity onPress={()=>{setRewardModal(true)}}><Image source={require('../../../assets/rewards2.png')} style={{ width: width / 2-20, height: width / 3 }} resizeMode='contain' /></TouchableOpacity>
-                        <TouchableOpacity onPress={()=>{setRewardModal(true)}}><Image source={require('../../../assets/rewards3.png')} style={{ width: width / 2-20, height: width / 3 }} resizeMode='contain' /></TouchableOpacity>
-                        <TouchableOpacity onPress={()=>{setRewardModal(true)}}><Image source={require('../../../assets/rewards3.png')} style={{ width: width / 2-20, height: width / 3 }} resizeMode='contain' /></TouchableOpacity>
-                        <TouchableOpacity onPress={()=>{setRewardModal(true)}}><Image source={require('../../../assets/rewards3.png')} style={{ width: width / 2-20, height: width / 3 }} resizeMode='contain' /></TouchableOpacity>
-                        <TouchableOpacity onPress={()=>{setRewardModal(true)}}><Image source={require('../../../assets/rewards3.png')} style={{ width: width / 2-20, height: width / 3 }} resizeMode='contain' /></TouchableOpacity>
-                        <TouchableOpacity onPress={()=>{setRewardModal(true)}}><Image source={require('../../../assets/rewards3.png')} style={{ width: width / 2-20, height: width / 3 }} resizeMode='contain' /></TouchableOpacity>
-                        <TouchableOpacity onPress={()=>{setRewardModal(true)}}><Image source={require('../../../assets/rewards3.png')} style={{ width: width / 2-20, height: width / 3 }} resizeMode='contain' /></TouchableOpacity>
+                    <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between', alignContent: 'center', }}>
+                        <TouchableOpacity onPress={() => { setRewardModal(true) }}>
+                            <Image source={require('../../../assets/rewards2.png')} style={{ width: width / 2 - 20, height: width / 3 }} resizeMode='contain' /></TouchableOpacity>
+                        <TouchableOpacity onPress={() => { setRewardModal(true) }}>
+                            <Image source={require('../../../assets/rewards2.png')} style={{ width: width / 2 - 20, height: width / 3 }} resizeMode='contain' /></TouchableOpacity>
+                        <TouchableOpacity onPress={() => { setRewardModal(true) }}>
+                           <RewardCard/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => { setRewardModal(true) }}>
+                           <RewardCard/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => { setRewardModal(true) }}>
+                           <RewardCard/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => { setRewardModal(true) }}>
+                           <RewardCard/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => { setRewardModal(true) }}>
+                           <RewardCard/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => { setRewardModal(true) }}>
+                           <RewardCard/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => { setRewardModal(true) }}>
+                           <RewardCard/>
+                        </TouchableOpacity>
                     </View>
                 </View>
             </ScrollView>
@@ -88,8 +126,8 @@ export default function Reward({ navigation }) {
                 onSwipeComplete={() => { setRewardModal(false) }}
                 onBackdropPress={() => { setRewardModal(false) }}
                 style={{ margin: 30, justifyContent: "center", }}>
-                    <Image source={require('../../../assets/rewards3.png')} style={{ width: width -50, height: width / 2 }} resizeMode='contain' />
-                </Modal>
+                <Image source={require('../../../assets/rewards3.png')} style={{ width: width - 50, height: width / 2 }} resizeMode='contain' />
+            </Modal>
             <Modal
                 isVisible={modal}
                 hasBackdrop={true}
@@ -101,15 +139,15 @@ export default function Reward({ navigation }) {
                 onBackdropPress={() => { setModal(false) }}
                 style={{ margin: 30, justifyContent: "center", }}>
                 <View style={{ backgroundColor: '#ffffff', padding: 20, borderRadius: 15, display: 'flex', }}>
-                     <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center', alignItems: 'center' }}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center', alignItems: 'center' }}>
                         <Text style={{ color: '#635E5E', textAlign: 'center', fontSize: 16, fontWeight: '500', marginVertical: 10 }}></Text>
                         <TouchableOpacity onPress={() => { setModal(false) }}>
                             <Ionicons name="close" size={30} color={'#000000'} />
                         </TouchableOpacity>
                     </View>
-                    <View style={{flexDirection: 'row', alignContent: 'center', alignItems: 'flex-start', justifyContent: 'flex-start'}}>
+                    <View style={{ flexDirection: 'row', alignContent: 'center', alignItems: 'flex-start', justifyContent: 'flex-start' }}>
                         <Image source={require('../../../assets/rewards6.png')} style={{ width: width / 4, height: width / 4 }} resizeMode='contain' />
-                        <View style={{flex: 1, marginLeft: 20}}>
+                        <View style={{ flex: 1, marginLeft: 20 }}>
                             <Text style={{ color: '#000000', textAlign: 'left', fontSize: 14, fontWeight: '600', marginTop: 10 }}>How to earn more coins</Text>
                             <Text style={{ color: '#9E9E9E', textAlign: 'left', fontSize: 12, marginTop: 5 }}>
                                 Take services from Homvery more frequently and earn more coin. Go through tips and tricks section and know more about it.
@@ -117,8 +155,8 @@ export default function Reward({ navigation }) {
                         </View>
 
                     </View>
-                    <View style={{flexDirection: 'row', alignContent: 'center', alignItems: 'flex-start', justifyContent: 'flex-start', marginTop: 20}}>
-                        <View style={{flex: 1, marginLeft: 20}}>
+                    <View style={{ flexDirection: 'row', alignContent: 'center', alignItems: 'flex-start', justifyContent: 'flex-start', marginTop: 20 }}>
+                        <View style={{ flex: 1, marginLeft: 20 }}>
                             <Text style={{ color: '#000000', textAlign: 'left', fontSize: 14, fontWeight: '600', marginTop: 10 }}>How to earn more coins</Text>
                             <Text style={{ color: '#9E9E9E', textAlign: 'left', fontSize: 12, marginTop: 5 }}>
                                 Take services from Homvery more frequently and earn more coin. Go through tips and tricks section and know more about it.
